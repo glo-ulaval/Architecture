10282163dd6777fa089f6171be7f21307f312d8a
Philippe Bourdages <philippe.bourdages@gmail.com>
2012-09-21 17:06:11 -0400

    + Ajout du repository pour les Courses

---
+++ schedulemanager/src/main/java/cours/ulaval/glo4003/repository/CourseRepository.java
@@ -0,0 +1,27 @@
+
+
+package cours.ulaval.glo4003.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import cours.ulaval.glo4003.model.Course;
+
+public class CourseRepository {
+
+	@Autowired
+	@Qualifier("CourseBeanRetriever")
+	private ICourseRetriever courseRetriever;
+
+	public void setCourseRetriever(ICourseRetriever courseRetriever) {
+		this.courseRetriever = courseRetriever;
+	}
+	
+	public ICourseRetriever getCourseRetriver() {
+		return courseRetriever;
+	}
+
+	public Iterable<Course> getAll() {
+		return courseRetriever.getCourses();
+	}
+}
---
+++ schedulemanager/src/test/java/cours/ulaval/glo4003/repository/CourseRepositoryTest.java
@@ -0,0 +1,17 @@
+package cours.ulaval.glo4003.repository;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CourseRepositoryTest {
+	
+	private CourseRepository courseRepository;
+
+	@Before
+	public void setUp() throws Exception {
+		courseRepository = new CourseRepository();
+	}
+
+}
