6bab968b2c3da62d98f7104894003a6f11a67ff8
Supra128 <vincent.seguin.1@ulaval.ca>
2012-09-21 17:07:22 -0400

    + Ajout de tests pour le retriever, tres basique

--- schedulemanager/src/test/java/cours/ulaval/glo4003/persistence/CourseBeanRetrieverUnitTest.java
+++ schedulemanager/src/test/java/cours/ulaval/glo4003/persistence/CourseBeanRetrieverUnitTest.java
@@ -1,11 +1,20 @@
 package cours.ulaval.glo4003.persistence;
 
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
 import org.junit.Test;
+import org.springframework.oxm.Marshaller;
 
 public class CourseBeanRetrieverUnitTest {
 
 	@Test
-	public void canInstantiateCourseBeanRetriever() {
+	public void canSetMarshallerInRetriever() {
+		Marshaller marshaller = mock(Marshaller.class);
 		CourseBeanRetriever retriever = new CourseBeanRetriever();
+
+		retriever.setMarshaller(marshaller);
+
+		assertEquals(marshaller, retriever.getMarshaller());
 	}
 }
