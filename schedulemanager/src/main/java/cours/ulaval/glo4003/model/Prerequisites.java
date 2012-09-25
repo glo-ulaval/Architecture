1b46fc64b931ec147e55c4e718234170e3f1e04b
Supra128 <vincent.seguin.1@ulaval.ca>
2012-09-25 10:28:43 -0400

    + Iterable sur Prerequisites

--- schedulemanager/src/main/java/cours/ulaval/glo4003/model/Prerequisites.java
+++ schedulemanager/src/main/java/cours/ulaval/glo4003/model/Prerequisites.java
@@ -1,24 +1,35 @@
 package cours.ulaval.glo4003.model;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
 
 @XmlType
-public class Prerequisites {
+public class Prerequisites implements Iterable {
 
 	private ConditionType condition;
 
 	@XmlElement(name = "prerequisites")
-	private ArrayList<Prerequisite> prerequisites = new ArrayList<Prerequisite>();
+	private List<Prerequisite> prerequisites = new ArrayList<Prerequisite>();
 
 	public ConditionType getCondition() {
 		return condition;
 	}
 
-	public ArrayList<Prerequisite> getPrerequisites() {
+	public List<Prerequisite> getPrerequisites() {
 		return prerequisites;
+	}
+
+	public void setPrerequisites(List<Prerequisite> list) {
+		this.prerequisites = list;
+	}
+
+	@Override
+	public Iterator<Prerequisite> iterator() {
+		return prerequisites.iterator();
 	}
 
 }
