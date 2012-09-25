6fef644a0db28a1c8c0e80f5e9ab696de07944f3
Supra128 <vincent.seguin.1@ulaval.ca>
2012-09-24 20:16:48 -0400

    ! A few warnings

--- schedulemanager/src/test/java/cours/ulaval/glo4003/repository/CourseOfferingRepositoryTest.java
+++ schedulemanager/src/test/java/cours/ulaval/glo4003/repository/CourseOfferingRepositoryTest.java
@@ -3,8 +3,6 @@ package cours.ulaval.glo4003.repository;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,28 +18,27 @@ public class CourseOfferingRepositoryTest {
 	private Map<String, CourseOffering> courseOfferings;
 	private CourseOfferingRepository courseOfferingRepository;
 	private ICourseOfferingRetriever offeringRetriever;
-	
-	
+
 	@Before
 	public void setUp() {
 		createOfferings();
 		offeringRetriever = mock(ICourseOfferingRetriever.class);
 		when(offeringRetriever.getOfferings()).thenReturn(courseOfferings);
-	
+
 		courseOfferingRepository = new CourseOfferingRepository();
 		courseOfferingRepository.setOfferingRetriever(offeringRetriever);
 	}
-	
+
 	@Test
 	public void canGetAllCourseOfferings() {
 		assertEquals(OFFERING_SIZE, courseOfferingRepository.getAll().size());
 	}
-	
+
 	private void createOfferings() {
 		courseOfferings = new HashMap<String, CourseOffering>();
-		for (Integer i=0; i < OFFERING_SIZE; i++){
-			courseOfferings.put(OFFERING_YEAR + i.toString(),mock(CourseOffering.class));
+		for (Integer i = 0; i < OFFERING_SIZE; i++) {
+			courseOfferings.put(OFFERING_YEAR + i.toString(), mock(CourseOffering.class));
 		}
 	}
-	
+
 }
