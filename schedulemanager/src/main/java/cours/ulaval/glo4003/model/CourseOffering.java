00ed6c4252a4b4f98b6e1ac4a51371d6b66e1ef2
Olivier Sylvain <g.oliviersylvain@gmail.com>
2012-09-27 15:09:36 -0400

    *Modification du test et de la classe de CourseOffering pour l'adapter à la nouvelle architecture

--- schedulemanager/src/main/java/cours/ulaval/glo4003/model/CourseOffering.java
+++ schedulemanager/src/main/java/cours/ulaval/glo4003/model/CourseOffering.java
@@ -1,10 +1,11 @@
 package cours.ulaval.glo4003.model;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class CourseOffering {
 
-	private Collection<Course> offering;
+	private Collection<Course> offering = new ArrayList<Course>();
 	private String year; // 2012-2013 from autumn ?
 	
 	public Collection<Course> getOffering() {
--- schedulemanager/src/test/java/cours/ulaval/glo4003/model/CourseOfferingTest.java
+++ schedulemanager/src/test/java/cours/ulaval/glo4003/model/CourseOfferingTest.java
@@ -1,16 +1,64 @@
 package cours.ulaval.glo4003.model;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.junit.Before;
 import org.junit.Test;
+
 
 public class CourseOfferingTest {
 	
+	private static final int NUMBER_OF_COURSES = 5;
+	CourseOffering courseOffering;
+	
+	@Before
+	public void setUp() {
+		courseOffering = new CourseOffering();
+	}
+	
 	@Test
 	public void canInstantiateCourseOffering() {
-		CourseOffering courseOffering = new CourseOffering();
-		
 		assertNotNull(courseOffering);
 		assertTrue(courseOffering instanceof CourseOffering);
 	}
+	
+	@Test
+	public void canGetYear() {
+		String goodYear = "2012-2013";
+		courseOffering.setYear(goodYear);
+		
+		assertEquals(goodYear, courseOffering.getYear());
+	}
+	
+	@Test
+	public void canGetOffering() {
+		Collection<Course> offering = createValidOffering();
+		courseOffering.setOffering(offering);
+		
+		assertEquals(offering, courseOffering.getOffering());
+	}
+	
+	@Test
+	public void canGetEmptyOfferingWhenDefault() {//TODO : À RENOMMER? pas d'idée...
+
+		assertEquals(0, courseOffering.getOffering().size());
+	}
+	
+	private ArrayList<Course> createValidOffering() {
+		Course course = mock(Course.class);
+		ArrayList<Course> offering = new ArrayList<Course>();
+		
+		for(int i = 0; i < NUMBER_OF_COURSES; i++) {
+			offering.add(course);
+		}
+		
+		return offering;
+	}
+
 }
