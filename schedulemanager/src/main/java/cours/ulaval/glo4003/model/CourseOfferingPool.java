09235f763c152665ea0077e7967f09f8e9049776
Supra128 <vincent.seguin.1@ulaval.ca>
2012-09-28 09:02:24 -0400

    * Changement de signature de methode

--- schedulemanager/src/main/java/cours/ulaval/glo4003/model/CourseOfferingPool.java
+++ schedulemanager/src/main/java/cours/ulaval/glo4003/model/CourseOfferingPool.java
@@ -2,24 +2,23 @@ package cours.ulaval.glo4003.model;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class CourseOfferingPool {
 
-	Map<String, ArrayList<String>> courseOfferings = new HashMap<String, ArrayList<String>>(); // <année, listeDesCours>
+	Map<String, ArrayList<String>> courseOfferings = new HashMap<String, ArrayList<String>>();
 
 	public void setCourseOfferings(HashMap<String, ArrayList<String>> offerings) {
 		courseOfferings = offerings;
 	}
 
-	public Object getOffering(String year) {
-		
+	public List<String> getOffering(String year) {
 		return courseOfferings.get(year);
 	}
 
 	public int getOfferingCount() {
 		return courseOfferings.size();
 	}
-	
-	
+
 }
