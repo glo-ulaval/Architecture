c3cad15d0532a9999857634c648285e7a583a3dd
Bruno Gagnon <bgagnonadam@gmail.com>
2012-09-29 14:25:39 -0400

    + Création de l'interface OfferingRepository.

---
+++ schedulemanager/src/main/java/cours/ulaval/glo4003/model/OfferingRepository.java
@@ -0,0 +1,15 @@
+package cours.ulaval.glo4003.model;
+
+import java.util.ArrayList;
+
+public interface OfferingRepository {
+	
+	public ArrayList<String> findYears();
+	
+	public Offering find(String year);
+	
+	public void store(Offering offering);
+	
+	public void delete(String year);
+
+}
