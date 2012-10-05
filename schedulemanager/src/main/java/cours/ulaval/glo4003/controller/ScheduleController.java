db402bff5fbd453bf3018f9a953469082675413e
Supra128 <vincent.seguin.1@ulaval.ca>
2012-10-05 15:00:21 -0400

    + Ajout de methode pour les schedules

--- schedulemanager/src/main/java/cours/ulaval/glo4003/controller/ScheduleController.java
+++ schedulemanager/src/main/java/cours/ulaval/glo4003/controller/ScheduleController.java
@@ -1,6 +1,7 @@
 package cours.ulaval.glo4003.controller;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import cours.ulaval.glo4003.model.TimeSlot;
 
 @Controller
 @RequestMapping(value = "/schedule")
@@ -27,8 +30,13 @@ public class ScheduleController {
 	@RequestMapping(value = "/{year}", method = RequestMethod.GET)
 	public ModelAndView scheduleByYear(@PathVariable String year)
 			throws Exception {
+		List<TimeSlot> slots = new ArrayList<TimeSlot>();
+		TimeSlot slot = new TimeSlot(Calendar.getInstance(), 2);
+		slots.add(slot);
+
 		ModelAndView mv = new ModelAndView("schedulebyyear");
 		mv.addObject("year", year);
+		mv.addObject("slots", slots);
 
 		return mv;
 	}
