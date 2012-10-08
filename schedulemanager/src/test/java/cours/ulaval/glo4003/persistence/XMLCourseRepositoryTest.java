867a3e99a13bfbd6b5ca7472cce73bcfd3e3bbb0
Supra128 <vincent.seguin.1@ulaval.ca>
2012-10-08 08:51:53 -0400

    + Ajout de tests pour le course repository

--- schedulemanager/src/test/java/cours/ulaval/glo4003/persistence/XMLCourseRepositoryTest.java
+++ schedulemanager/src/test/java/cours/ulaval/glo4003/persistence/XMLCourseRepositoryTest.java
@@ -4,48 +4,65 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import cours.ulaval.glo4003.model.Course;
+import cours.ulaval.glo4003.model.Offering;
 import cours.ulaval.glo4003.utils.ConfigManager;
 
 public class XMLCourseRepositoryTest {
 
 	private static final String AN_ACRONYM = "GLO-4003";
 
+	private XMLCourseRepository repository;
+
+	@Before
+	public void setUp()
+			throws Exception {
+		XMLSerializer<CoursesDTO> mockedSerializer = mock(XMLSerializer.class);
+		CoursesDTO dto = prepareMockedCourseDTO();
+		ConfigManager resourcesPaths = ConfigManager.getConfigManager();
+		when(mockedSerializer.deserialize(resourcesPaths.getCoursesFilePath())).thenReturn(dto);
+
+		repository = new XMLCourseRepository(mockedSerializer);
+	}
+
 	@Test
-	public void canInstantiateCourseRepository() throws Exception {
+	public void canInstantiateCourseRepository()
+			throws Exception {
 		XMLCourseRepository repository = new XMLCourseRepository();
 
 		assertNotNull(repository);
 	}
 
 	@Test
-	public void canFindAllCourses() throws Exception {
-		XMLSerializer<CoursesDTO> mockedSerializer = mock(XMLSerializer.class);
-		CoursesDTO dto = new CoursesDTO();
-		List<Course> courses = new ArrayList<Course>();
-		ConfigManager resourcesPaths = ConfigManager.getConfigManager();
-		when(mockedSerializer.deserialize(resourcesPaths.getCoursesFilePath())).thenReturn(dto);
-
-		XMLCourseRepository repository = new XMLCourseRepository(mockedSerializer);
-
-		assertEquals(courses, repository.findAll());
+	public void canFindAllCourses()
+			throws Exception {
+		assertEquals(1, repository.findAll().size());
 	}
 
 	@Test
-	public void canFindACourseByAcronym() throws Exception {
-		XMLSerializer<CoursesDTO> mockedSerializer = mock(XMLSerializer.class);
-		CoursesDTO dto = prepareMockedCourseDTO();
-		ConfigManager resourcesPaths = ConfigManager.getConfigManager();
-		when(mockedSerializer.deserialize(resourcesPaths.getCoursesFilePath())).thenReturn(dto);
-		XMLCourseRepository repository = new XMLCourseRepository(mockedSerializer);
-
+	public void canFindACourseByAcronym()
+			throws Exception {
 		Course course = repository.findByAcronym(AN_ACRONYM);
 
 		assertEquals(AN_ACRONYM, course.getAcronym());
+	}
+
+	@Test
+	public void canFindCoursesByOffering()
+			throws Exception {
+		Offering offering = mock(Offering.class);
+		when(offering.getAcronyms()).thenReturn(Arrays.asList(AN_ACRONYM));
+
+		List<Course> courses = repository.findByOffering(offering);
+
+		assertEquals(1, courses.size());
+		assertEquals(AN_ACRONYM, courses.get(0).getAcronym());
 	}
 
 	private CoursesDTO prepareMockedCourseDTO() {
