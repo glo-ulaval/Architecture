563131d197d4d195691e0fae43fd22b73323bbae
jrochette <rochette.jonathan@gmail.com>
2012-10-08 14:34:26 -0400

    * Modification de ScheduleDTO pour la sérialization et ajout de tests unitaires

--- schedulemanager/src/main/java/cours/ulaval/glo4003/persistence/ScheduleDTO.java
+++ schedulemanager/src/main/java/cours/ulaval/glo4003/persistence/ScheduleDTO.java
@@ -1,6 +1,23 @@
 package cours.ulaval.glo4003.persistence;
 
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import cours.ulaval.glo4003.model.Schedule;
+
+@XmlRootElement(name = "schedules")
 public class ScheduleDTO {
+	private List<Schedule> schedules = new ArrayList<Schedule>();
 
+	public void setSchedules(List<Schedule> schedules) {
+		this.schedules = schedules;
+	}
+
+	@XmlElement(name = "schedule")
+	public List<Schedule> getSchedules() {
+		return this.schedules;
+	}
 }
