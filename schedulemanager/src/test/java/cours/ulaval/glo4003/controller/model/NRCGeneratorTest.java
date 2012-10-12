81a063fcd5a13b22cdde57fa377ffc0165c6a59d
Supra128 <vincent.seguin.1@ulaval.ca>
2012-10-12 13:58:49 -0400

    ! Annotation manquante

--- schedulemanager/src/test/java/cours/ulaval/glo4003/controller/model/NRCGeneratorTest.java
+++ schedulemanager/src/test/java/cours/ulaval/glo4003/controller/model/NRCGeneratorTest.java
@@ -2,8 +2,11 @@ package cours.ulaval.glo4003.controller.model;
 
 import static org.junit.Assert.*;
 
+import org.junit.Test;
+
 public class NRCGeneratorTest {
 
+	@Test
 	public void canGenerate() {
 		Integer generatedNrc = NRCGenerator.generate();
 
