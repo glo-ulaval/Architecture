4cdf9dc0d1290e84ca79bfe7ef23fc6fa1e15eff
jrochette <rochette.jonathan@gmail.com>
2012-11-12 15:36:15 -0500

    *Ajout de la méthode isAvailable() dans Availability *Ajout de la méthode generateAvailabiltyConflict dans Section +Ajout du UnavailableTeacherFilter

--- schedulemanager/src/main/java/cours/ulaval/glo4003/domain/Availability.java
+++ schedulemanager/src/main/java/cours/ulaval/glo4003/domain/Availability.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import cours.ulaval.glo4003.controller.model.AvailabilityModel;
+import cours.ulaval.glo4003.domain.TimeSlot.DayOfWeek;
 
 public class Availability {
 
@@ -27,6 +28,36 @@ public class Availability {
 		friday = availability.getFriday();
 
 		this.idul = idul;
+	}
+
+	public List<AvailabilityLevel> getListWithDay(DayOfWeek day) {
+		switch (day) {
+		case FRIDAY:
+			return friday;
+		case MONDAY:
+			return monday;
+		case THURSDAY:
+			return thursday;
+		case TUESDAY:
+			return tuesday;
+		case WEDNESDAY:
+			return wednesday;
+		default:
+			return null;
+		}
+	}
+
+	public AvailabilityLevel isAvailableForTimeSlot(TimeSlot timeSlot) {
+		int timeSlotBeginningIndex = timeSlot.getStartTime().getHour() - 8;
+		List<AvailabilityLevel> day = getListWithDay(timeSlot.getDayOfWeek());
+		for (int i = 0; i < timeSlot.getDuration(); i++) {
+			if (day.get(timeSlotBeginningIndex + i) == AvailabilityLevel.Unavailable) {
+				return AvailabilityLevel.Unavailable;
+			} else if (day.get(timeSlotBeginningIndex + i) == AvailabilityLevel.PreferedNotAvailable) {
+				return AvailabilityLevel.PreferedNotAvailable;
+			}
+		}
+		return AvailabilityLevel.Available;
 	}
 
 	public String getIdul() {
@@ -88,4 +119,5 @@ public class Availability {
 
 		return model;
 	}
+
 }
--- schedulemanager/src/test/java/cours/ulaval/glo4003/domain/SectionTest.java
+++ schedulemanager/src/test/java/cours/ulaval/glo4003/domain/SectionTest.java
@@ -1,6 +1,8 @@
 package cours.ulaval.glo4003.domain;
 
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -11,7 +13,10 @@ import org.junit.Test;
 import cours.ulaval.glo4003.domain.TimeSlot.DayOfWeek;
 import cours.ulaval.glo4003.domain.conflictdetection.conflict.ConcomittingCoursesConflict;
 import cours.ulaval.glo4003.domain.conflictdetection.conflict.Conflict;
+import cours.ulaval.glo4003.domain.conflictdetection.conflict.DisponibilityConflict;
 import cours.ulaval.glo4003.domain.conflictdetection.conflict.SameLevelCourseConflict;
+import cours.ulaval.glo4003.domain.conflictdetection.conflict.UnavailableTeacherConflict;
+import cours.ulaval.glo4003.domain.repository.AvailabilityRepository;
 
 public class SectionTest {
 	private static final int A_HOUR = 10;
@@ -39,7 +44,8 @@ public class SectionTest {
 		courseTimeSlot = Arrays.asList(new TimeSlot(generateCourseTimeSlotStartTime(), 3, DayOfWeek.MONDAY));
 		labTimeSlot = new TimeSlot(generateCourseTimeSlotStartTime(), 3, DayOfWeek.FRIDAY);
 		courseAcronym = "GLO_4002";
-		section = new Section(nrc, group, personInCharge, teachers, teachMode, timeDedicated, courseAcronym, courseTimeSlot, labTimeSlot);
+		section = new Section(nrc, group, personInCharge, teachers, teachMode, timeDedicated, courseAcronym, courseTimeSlot,
+				labTimeSlot);
 	}
 
 	@Test
@@ -66,7 +72,7 @@ public class SectionTest {
 	public void weCanFindATeacherInTheList() {
 		assertTrue(section.hasTeacher("teacher1"));
 	}
-	
+
 	@Test
 	public void canGenerateConcomittingCourseConflicts() {
 		List<Conflict> conflicts = section.generateConcomittingConflicts(section);
@@ -85,6 +91,21 @@ public class SectionTest {
 		for (int i = 0; i < conflicts.size(); i++) {
 			assertTrue(conflicts.get(i) instanceof SameLevelCourseConflict);
 		}
+	}
+
+	@Test
+	public void canGenerateUnavailableTeacherConflict() {
+		Availability availabilityMock = mock(Availability.class);
+		when(availabilityMock.isAvailableForTimeSlot(any(TimeSlot.class))).thenReturn(AvailabilityLevel.Unavailable,
+				AvailabilityLevel.PreferedNotAvailable);
+		AvailabilityRepository repositoryMock = mock(AvailabilityRepository.class);
+		when(repositoryMock.findByIdul(anyString())).thenReturn(availabilityMock);
+
+		List<Conflict> conflicts = section.generateUnavailableTeacherConflicts(repositoryMock);
+
+		assertEquals(4, conflicts.size());
+		assertTrue(conflicts.get(0) instanceof UnavailableTeacherConflict);
+		assertTrue(conflicts.get(1) instanceof DisponibilityConflict);
 	}
 
 	private Time generateCourseTimeSlotStartTime() {
